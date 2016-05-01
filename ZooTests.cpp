46a89a0ca76e41e6cf4572c2819e586fd5552a10
Adam Stevenson <adamstevenson121@gmail.com>
2016-05-01 16:51:05 -0400

    Tests for new syntax and tags

--- ZooTests.cpp
+++ ZooTests.cpp
@@ -1,5 +1,5 @@
 #define CATCH_CONFIG_MAIN
-#include "catch.hpp" //requires Catch
+#include "catch.hpp"
 #include "zoo.h"
 
 using namespace zoo;
@@ -39,17 +39,21 @@ TEST_CASE("Nodes can be modified") {
 
 TEST_CASE("Documents can be modified") {
 	HTMLDocument doc;
-	doc.add(Tag{ "header",
-		Tag{ "h1",
-			Text{"Hello World"}}});
+	doc.head(
+		Tag( "title",
+			Text("Hello World" )));
+	doc.body(
+		Div(
+			H1(Text("Heading")),
+			H2(Text("Subheading"))),
+		Div(
+			Div(Strong(Text("a list"))),
+			Ul(
+				Li(Text("item 1")),
+				Li(Text("item 2")),
+				Li(Text("item 3")))));
 
-	doc.add(Tag{ "content",
-		Tag{"b",
-			Text{"this is bold text"}},
-		Tag{"i",
-			Text{"this is italic text"}}
-	});
-	REQUIRE(doc.toString() == "<!DOCTYPE html><html><head></head><body><header><h1>Hello World</h1></header><content><b>this is bold text</b><i>this is italic text</i></content></body></html>");
+	REQUIRE(doc.toString() == "<!DOCTYPE html><html><head><title>Hello World</title></head><body><div><h1>Heading</h1><h2>Subheading</h2></div><div><div><strong>a list</strong></div><ul><li>item 1</li><li>item 2</li><li>item 3</li></ul></div></body></html>");
 
 }
 
